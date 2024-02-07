80e739643502ee2d062387e95a92f442881bb08c
Grace Anyango <110466244+GraceAnyango@users.noreply.github.com>
2024-02-07 14:26:10 +0300

    initial commits

---
+++ index.py
@@ -0,0 +1,17 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Show the range between the 4th and 7th elements
+range_4th_to_7th = my_list[3:7]
+print("Range between the 4th and 7th elements:", range_4th_to_7th)
+
+
+
+list_of_lists = [
+    [1, 2, 3],
+    ['a', 'b', 'c'],
+    [True, False, True]
+]
+
+my_dict = {'name' : 'grace', 'age': '14'}
+
+
